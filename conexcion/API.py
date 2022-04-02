13590f53166eb63776eaab62d6bc75a4016d59cf
echodegrasa <74998947+issacbrizuela1@users.noreply.github.com>
2022-04-01 19:25:35 -0600

    api,confjson,config
    1.se agrego confjson y configuracion en una carpeta pra acomodar mejor los archivos de configuracion.
    2.config.json es para guardar los datos de sesion.
    3.se modificaron la importacion y datos en api.

--- conexcion/API.py
+++ conexcion/API.py
@@ -1,30 +1,37 @@
-p = os.path.abspath('../configuracion.py')
-sys.path.insert(1, p)
 from urllib import response
-import requests 
+import requests
 import json
-import sys,os
+import sys
+import os
 from requests.structures import CaseInsensitiveDict
-import configuracion
+from conf.configuracion import conf
+
+
 class api:
     def __init__(self):
-        
-        tabla=configuracion.URL
-        dbuser=configuracion.USUARIO
-        dbpassword=configuracion.PASSWORD
-        usuario=configuracion.U_USUARIO
-        password=configuracion.U_PASSWORD
-        HEADEr=''
-        token=''
-        datos=list()
-        path=''
-    def GET(self,header,token,datos,path):
-        response=requests.get(self.tabla+path)
-    def POST(self,header,token,datos,path):
-        response=requests.post(self.tabla+path)
-    def PUT(self,header,token,datos,path):
-        response=requests.put(self.tabla+path)
-    def DEL(self,header,token,datos,path):
-        response=requests.delete(self.tabla+path)
-s=api.tabla
+
+        tabla = conf.URL
+        dbuser = conf.USUARIO
+        dbpassword = conf.PASSWORD
+        usuario = conf.U_USUARIO
+        password = conf.U_PASSWORD
+        HEADEr = ''
+        token = ''
+        datos = list()
+        path = ''
+
+    def GET(self, header, token, datos, path):
+        response = requests.get(self.tabla+path)
+
+    def POST(self, header, token, datos, path):
+        response = requests.post(self.tabla+path)
+
+    def PUT(self, header, token, datos, path):
+        response = requests.put(self.tabla+path)
+
+    def DEL(self, header, token, datos, path):
+        response = requests.delete(self.tabla+path)
+
+
+s = api.tabla
 print(s)
