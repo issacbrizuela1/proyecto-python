9e4598c6de5c3aff712c0e3f8c03e2df882c679a
echodegrasa <74998947+issacbrizuela1@users.noreply.github.com>
2022-04-03 12:04:09 -0500

    add,change,modifi
    1.se agrego el archivo de pruebas a gitignore.
    2.se movio sensoresjson,sesionjson a la carpeta conf.
    3.se modifico el archivo historial, se agrego el crud aqui no se almacenara nada en local.
    4.mhistorial posiblemente lo haga con mvc.
    5. se renombro el archivo de pruebas

--- historial/historial.py
+++ historial/historial.py
@@ -1,15 +1,55 @@
-
+import collections
 from configuracion import Configuracion
-
-
+import gc
+import datetime
+from pymongo import MongoClient
+import pprint
 class Historial(Configuracion):
     def __init__(self) -> None:
         super().__init__()
         url = self.URL
         client = MongoClient(url)
-        collection = client[self.DB]['sensores']
+        collection = client[self.DB]['historialsensores']
         id=0
         idSensor=0
         Valor=list()
         Fechadecreacion=""
         Fechadeactualizacion=""
+        lista = list()
+    #gets
+    def getHistorial(self):
+        r = self.collection.find({'idSensor': 1})
+        t=self.collection.count_documents({'idSensor': 1})
+        pp = pprint.PrettyPrinter(indent=4)
+        for s in range(t):
+            pp.pprint(r[s])
+    def getlistaHistorial(self,index):
+        r = self.collection.find({'idSensor': 1})
+        print(r[index])
+    def getallList(self):
+        print(len(self.lista))
+        for s in self.lista:
+            print(s)
+
+    #adds
+    def addListHistorial(self):
+        r = self.collection.find({'idSensor': 1})
+        t=self.tamano()
+        for s in range(t):
+            self.lista.append(r[s])
+    def addMongoHistorial(self, datos):
+        self.collection.insert_one(datos)
+    #del
+    def eliminarMongoHistorial(self, datos):
+        self.collection.delete_one(datos)
+    def eliminarList(self,sensor):
+        self.lista.remove(sensor)
+    #mod
+    def modificarHistorial(self, datos, index):
+        self.collection.update_one(index, datos)
+    #tamaño
+    def tamanoMongoHistorial(self):
+        return self.collection.count_documents({'idSensor': 1})
+p=Historial()
+p.addMongoHistorial()
+gc.enable()
