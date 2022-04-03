c9c481cdf802e3f8cd3d1f493701c25eb5c2b6b8
echodegrasa <74998947+issacbrizuela1@users.noreply.github.com>
2022-04-03 13:58:49 -0500

    update historial
    se eliminaron cosas que no se usarian

--- historial/historial.py
+++ historial/historial.py
@@ -1,55 +1,64 @@
 import collections
+
+import self
 from configuracion import Configuracion
 import gc
 import datetime
 from pymongo import MongoClient
 import pprint
-class Historial(Configuracion):
+
+
+class Historial():
+    url = Configuracion.URL
+    client = MongoClient(url)
+    collection = client[Configuracion.DB]['historialsensores']
     def __init__(self) -> None:
         super().__init__()
-        url = self.URL
+        url = Configuracion.URL
         client = MongoClient(url)
-        collection = client[self.DB]['historialsensores']
-        id=0
-        idSensor=0
-        Valor=list()
-        Fechadecreacion=""
-        Fechadeactualizacion=""
+        collection = client[Configuracion.DB]['historialsensores']
+        id = 0
+        idSensor = 0
+        Valor = list()
+        Fechadecreacion = ""
+        Fechadeactualizacion = ""
         lista = list()
-    #gets
+    # gets
+
     def getHistorial(self):
         r = self.collection.find({'idSensor': 1})
-        t=self.collection.count_documents({'idSensor': 1})
+        t = self.collection.count_documents({'idSensor': 1})
         pp = pprint.PrettyPrinter(indent=4)
         for s in range(t):
             pp.pprint(r[s])
-    def getlistaHistorial(self,index):
-        r = self.collection.find({'idSensor': 1})
-        print(r[index])
-    def getallList(self):
-        print(len(self.lista))
-        for s in self.lista:
-            print(s)
 
-    #adds
-    def addListHistorial(self):
-        r = self.collection.find({'idSensor': 1})
-        t=self.tamano()
-        for s in range(t):
-            self.lista.append(r[s])
+    # adds
     def addMongoHistorial(self, datos):
         self.collection.insert_one(datos)
-    #del
+    # del
+
     def eliminarMongoHistorial(self, datos):
         self.collection.delete_one(datos)
-    def eliminarList(self,sensor):
-        self.lista.remove(sensor)
-    #mod
+    # mod
+
     def modificarHistorial(self, datos, index):
         self.collection.update_one(index, datos)
-    #tamaño
+    # tamaño
+
     def tamanoMongoHistorial(self):
         return self.collection.count_documents({'idSensor': 1})
-p=Historial()
-p.addMongoHistorial()
+
+
+p = Historial()
+p.addMongoHistorial({
+    "id": 2,
+    "idSensor": 1,
+    "Valor":{
+        "temparatura":5,
+        "humedad":6
+    },
+    "Fechadecreacion": datetime.datetime.now(),
+    "Fechadeactualizacion": ""
+})
+p.getHistorial()
 gc.enable()
