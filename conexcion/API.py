a4dbbf3d59ee9c1e0224f9130df23eeae074c780
echodegrasa <74998947+issacbrizuela1@users.noreply.github.com>
2022-04-07 00:25:13 -0500

    update
    1. se modificaron los métodos para agregar control de errores.

--- conexcion/API.py
+++ conexcion/API.py
@@ -16,26 +16,74 @@ class api:
     def __init__(self):
         pass
 
-    def GET(self,path,datos):
-        if path=="login":
-            headers = CaseInsensitiveDict()
-            headers["Accept"] = "application/json"
-            resp = requests.post(path,data=datos, headers=headers)
-            return resp.json()
-        elif path!="":
+    def GET(self,url,path,datos):
+        try:
+            if path=="login":
+                headers = CaseInsensitiveDict()
+                headers["Accept"] = "application/json"
+                data = {
+                    "usuario":datos.usuario,
+                    "contraseña":datos.contrasena
+                }
+                resp = requests.post(path,data=data, headers=headers)
+                return resp.json()
+            elif path!="" or path!="register":
+                path=self.endpoint+path
+                headers = CaseInsensitiveDict()
+                headers["Accept"] = "application/json"
+                headers["Authorization"] = "Bearer %s" % self.token
+                resp = requests.get(url+path,data=datos, headers=headers)
+                return resp
+        except Exception as error:
+            print(error)
+
+    def POST(self,url,path):
+        #return requests.post(url+path)
+        try:
+            if path!="":
+                path=self.endpoint+path
+                data = {}
+                headers = CaseInsensitiveDict()
+                headers["Accept"] = "application/json"
+                headers["Authorization"] = "Bearer %s" % self.token
+                resp = requests.get(url+path,data=data, headers=headers)
+                return resp
+        except Exception as error:
+            print(error)
+
+
+    def PUT(self,url,path):
+        try:
+            if path!="":
+                path=self.endpoint+path
+                data = {}
+                headers = CaseInsensitiveDict()
+                headers["Accept"] = "application/json"
+                headers["Authorization"] = "Bearer %s" % self.token
+                resp = requests.get(url+path,data=data, headers=headers)
+                return resp
+        except Exception as error:
+            print(error)
+
+    def DEL(self,url,path):
+        try:
+            if path!="":
+                path=self.endpoint+path
+                data = {}
+                headers = CaseInsensitiveDict()
+                headers["Accept"] = "application/json"
+                headers["Authorization"] = "Bearer %s" % self.token
+                resp = requests.get(url+path,data=data, headers=headers)
+                return resp
+        except Exception as error:
+            print(error)
+
+    def getToken(self,credenciales,path="login"):
+        try:
             path=self.endpoint+path
-            data = {}
             headers = CaseInsensitiveDict()
             headers["Accept"] = "application/json"
-            headers["Authorization"] = "Bearer %s" % self.token
-            resp = requests.get(path,data=data, headers=headers)
-            return resp
-
-    def POST(self,path):
-        return requests.post(self.tabla+path)
-
-    def PUT(self,path):
-        return  requests.put(self.tabla+path)
-
-    def DEL(self,path):
-        return  requests.delete(self.tabla+path)
+            resp = requests.post(path,data=credenciales, headers=headers)
+            return resp.json()
+        except Exception as error:
+            print(error)
