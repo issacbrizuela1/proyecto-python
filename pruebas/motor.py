88d4fd9ac5c1565a6388d377883fdf7fd6d90fc4
echodegrasa <74998947+issacbrizuela1@users.noreply.github.com>
2022-04-10 23:39:24 -0500

    editar
    se edito el archivo de pruebas para el motor

--- pruebas/motor.py
+++ pruebas/motor.py
@@ -1,31 +1,31 @@
 import RPi.GPIO as GPIO
 from time import sleep
-
+ 
 GPIO.setmode(GPIO.BOARD)
-
-Motor1A = 16
-Motor1B = 18
-Motor1E = 22
-
-GPIO.setup(Motor1A, GPIO.OUT)
-GPIO.setup(Motor1B, GPIO.OUT)
-GPIO.setup(Motor1E, GPIO.OUT)
-
+ 
+Motor1A = 16#GPIO 23-13|pin 16 nueba ubicasion pasa al pin 29 L293d pin 7
+Motor1B = 18#GPIO 24-26|pin 18 nueba ubicasion pasa al pin 37 L293d pin 2
+Motor1E = 22#GPIO 25-19|pin 22 nueba ubicasion pasa al pin 31 L293d pin 1
+ 
+GPIO.setup(Motor1A,GPIO.OUT)
+GPIO.setup(Motor1B,GPIO.OUT)
+GPIO.setup(Motor1E,GPIO.OUT)
+ 
 print ("Going forwards")
-GPIO.output(Motor1A, GPIO.HIGH)
-GPIO.output(Motor1B, GPIO.LOW)
-GPIO.output(Motor1E, GPIO.HIGH)
-
+GPIO.output(Motor1A,GPIO.HIGH)
+GPIO.output(Motor1B,GPIO.LOW)
+GPIO.output(Motor1E,GPIO.HIGH)
+ 
 sleep(2)
-
+ 
 print ("Going backwards")
-GPIO.output(Motor1A, GPIO.LOW)
-GPIO.output(Motor1B, GPIO.HIGH)
-GPIO.output(Motor1E, GPIO.HIGH)
-
+GPIO.output(Motor1A,GPIO.LOW)
+GPIO.output(Motor1B,GPIO.HIGH)
+GPIO.output(Motor1E,GPIO.HIGH)
+ 
 sleep(2)
-
+ 
 print ("Now stop")
-GPIO.output(Motor1E, GPIO.LOW)
-
+GPIO.output(Motor1E,GPIO.LOW)
+ 
 GPIO.cleanup()
