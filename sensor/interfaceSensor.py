c0c59edb727536bf1bef12920883059c7cbaf6ec
echodegrasa <74998947+issacbrizuela1@users.noreply.github.com>
2022-04-11 20:26:45 -0500

    editar,crear
    se termino la interfaz de sensores

--- sensor/interfaceSensor.py
+++ sensor/interfaceSensor.py
@@ -1,11 +1,10 @@
-import json
-from operator import truediv
 import os
 import datetime
 from sensores import Sensor
 
 
 class InterfasSensor:
+    
     def __init__(self) -> None:
         self.lista = Sensor()
         self.lista.toObjects()
@@ -13,14 +12,14 @@ class InterfasSensor:
     def limpirarConsola(self):
         os.system('cls' if os.name == 'nt' else 'clear')
 
-    def crearNuevoSensor():
+    def crearNuevoSensor(self):
         s = Sensor()
-        ultimosensor=s.tamano()
-        s.idSensor=ultimosensor+1
-        s.idUsuario=""#usuario en sesion
-        s.NombreSensor = input("Nombre del Producto:")
-        s.Descripcion = input("Nombre del Producto:")
-        s.Estado[0]
+        ultimosensor = s.tamano()
+        s.idSensor = ultimosensor+1
+        s.idUsuario = ""  # usuario en sesion
+        s.NombreSensor = input("Nombre del Sensor:")
+        s.Descripcion = input("Descripción del Sensor:")
+        s.Estado="indefinido"
         print(
             "acontinuacion ingrese los puertos GPIO que usara y un nombre para cada puerto.")
         control = True
@@ -31,90 +30,132 @@ class InterfasSensor:
             s.GPIO.append({str(nombrePuerto): int(puerto)})
             if opcion.upper() == "Y":
                 control = False
-        s.Fechadecreacion = datetime.datetime.now()
-        s.IMG = input(
-            "ingrese una url de una imagen siquiere que su sensor tenga una\t:")
+        fecha=datetime.datetime.now()
+        s.Fechadecreacion = str(fecha)
+        s.IMG = input("ingrese una url de una imagen siquiere que su sensor tenga una\t:")
         return s
 
     def iterar(self, lista):
+        i = 0
         for valor in list(lista):
             if isinstance(valor, list):
                 for subval in list(valor):
                     self.iterar(subval)
             else:
-                print(valor)
+                print(str(i).ljust(5) + "\t\t" +str(valor))
+                i += 1
         pass
 
     def mostrarSensores(self, lista=None):
         self.limpirarConsola()
-        print("\n\n" + "*" * 30 + "Datos de Productos" + "*" * 30)
+        print("\n\n" + "*" * 30 + "Datos de Sensores" + "*" * 30)
         if (lista == None):
             mylista = self.lista
         else:
-            mylista = lista
-        print("NombreSensor".ljust(5) + "\t\t" +
-              'Descripcion'.ljust(20) + '\t\t' +
-              'Estado'.ljust(20) + '\t\t' +
-              'GPIO'.ljust(20) + '\t\t' +
-              'Fechadecreacion'.ljust(20) + '\t\t' +
-              'Fechadeactualisacion'.ljust(20) + '\t\t' +
-              'IMG'.ljust(20) + '\t\t')
-        i = 0
-        for p in mylista:
-            print(str(i).ljust(5) + "\t\t" + str(p))
-            i += 1
+            mylista = self.lista
+        print("#".ljust(5) + "\t\t" +
+              "idSensor".ljust(5) + "\t\t" +
+              "idUsuario".ljust(5) + "\t\t" +
+              "NombreSensor".ljust(5) + "\t\t" +
+              'Descripcion'.ljust(10) + '\t\t' +
+              'Estado'.ljust(10) + '\t\t' +
+              'GPIO'.ljust(10) + '\t\t' +
+              'Fechadecreacion'.ljust(10) + '\t\t' +
+              'Fechadeactualisacion'.ljust(10) + '\t\t' +
+              'IMG'.ljust(10) + '\t\t')
+        self.iterar(mylista)
         input("oprime enter para continuar .....")
 
     def buscarSensor(self):
         print("por medio de que desea buscar el sensor:\n"+"1.por id\n2.nommbre")
-        opcion=int(input(">>>"))
-        if opcion==1:
-            nombre=str(input("ingrese el nombre del sensor:"))
+        opcion = int(input(">>>"))
+        if opcion == 1:
+            nombre = str(input("ingrese el nombre del sensor:"))
             mylista = [p for p in self.lista if p.NombreSensor == nombre]
-        elif opcion==2:
-            id=str(input("ingrese el nombre del sensor:"))
+        elif opcion == 2:
+            id = str(input("ingrese el nombre del sensor:"))
             mylista = [p for p in self.lista if p.idSensor == id]
         self.mostrarSensores(mylista)
 
     def getListaSensores(self):
         return self.lista
+        
 
     def modificarSensor(self):
-        es=Sensor()
+        es = Sensor()
         print("si no desea modificar todo solo presione enter para omitir dependiendo del campo.")
-        id=int(input("ingrese el id del sensor: "))
-        mSensor=self.lista.mostrar_sensor(id)
-        nNombre=str("ingrese el nombre del producto: ")
+        id = int(input("ingrese el id del sensor: "))
+        mSensor = self.lista.mostrar_sensor(id)
+        nNombre = input("ingrese el nuevo nombre del sensor: ")
+        nDescripcion = input("ingrese la nuevo descripcion del sensor: ")
         if (len(nNombre) > 0):
-            mSensor.NombreSensor=nNombre
-        nDescripcion=str("ingrese el nombre del producto: ")
+            mSensor.NombreSensor = nNombre
         if (len(nDescripcion) > 0):
-            mSensor.Descripcion=nDescripcion
-        ind=0
+            mSensor.Descripcion = nDescripcion
+        ind = 0
         for x in es.Estados:
-            ind+=1
-            print(ind+"-"+x)
-        nEstado=int(input("seleccione el nuevo estado.\n elija el numero"))
+            ind += 1
+            print(str(ind)+"-"+str(x))
+        nEstado = int(input("seleccione el nuevo estado.\n elija el numero"))
         if (nEstado > 0):
-            mSensor.Estado=es.Estados[nEstado]
+            mSensor.Estado = es.Estados[nEstado]
         print(
             "acontinuacion ingrese los nuevos puertos GPIO que usara y un nombre para cada puerto.")
         control = True
-        GPIO=list()
+        GPIO = list()
         while control:
             nombrePuerto = input("nombre del puerto\t:")
             puerto = input("numero del puerto a usar\t:")
             opcion = input("¿Desea terminar de agregar puertos GPIO? Y/N")
             GPIO.append({str(nombrePuerto): int(puerto)})
             if opcion.upper() == "Y":
-                mSensor.GPIO=GPIO
+                mSensor.GPIO = GPIO
                 control = False
-        mSensor.Fechadeactualisacion=datetime.datetime.now()
-        mSensor.IMG=str(input("ingrese la nueva url para la imagen"))
+        fecha=datetime.datetime.now()
+        mSensor.Fechadeactualisacion = str(fecha)
+        mSensor.IMG = str(input("ingrese la nueva url para la imagen"))
         pass
-    
+
     def eliminarSensor(self):
-        id = input("Introduce ID:")
+        id = input("Introduce el #:")
         id = int(id)
         print(self.lista.mostrar_sensor(id))
         self.lista.eliminar(self.lista.mostrar_sensor(id))
+
+    def menuProductos(self):
+        a = 10
+        while a != 0:
+            self.limpirarConsola()
+            print("\n\n" + "*" * 30 + "Menu de Sensores" + "*" * 30)
+            print("1) Nuevo Sensor")
+            print("2) Modificar Sensor")
+            print("3) Eliminar Sensor")
+            print("4) Consultar Sensor")
+            print("5) Mostrar Sensores")
+            print("0) Salir")
+            a = input("Selecciona una opción: ")
+            if (a == '1'):
+                p = self.crearNuevoSensor()
+                self.lista.crear_sensor(p)
+                self.lista.toJson(self.lista)
+            elif (a == '2'):
+                self.mostrarSensores()
+                self.modificarSensor()
+                self.lista.toJson(self.lista)
+            elif (a == '3'):
+                self.mostrarSensores()
+                self.eliminarSensor()
+                self.lista.toJson(self.lista)
+            elif (a == '4'):
+                self.buscarSensor()
+            elif (a == '5'):
+                self.mostrarSensores()
+            elif (a == '0'):
+                break
+            else:
+                print(
+                    "La opcion no es correcta vuelve a seleccionar da enter para continuar.....")
+                input()
+if __name__ == '__main__':
+    s=InterfasSensor()
+    s.menuProductos()
--- sensor/sensores.py
+++ sensor/sensores.py
@@ -8,23 +8,25 @@ from jsonfileSensor import JsonFile
 
 
 class Sensor(JsonFile):
-
-    def __init__(self, url, db):
-        self.url = url
-        client = MongoClient(self.url)
-        self.db = db
-        self.collection = client[db]['sensores']
+    
+    url = 'mongodb+srv://root:ZXCVzxcv1234@sandbox1.1jic6.mongodb.net/proyecto?authSource=admin&replicaSet=atlas-lz7100-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+    client = MongoClient(url)
+    db = 'proyecto'
+    collection = client[db]['sensores']
+    Estados = ["indefinido", "habilitado", "deshabilitado"]
+    def __init__(self, idSensor=0, idUsuario=0, NombreSensor='', Descripcion='', Fechadecreacion='', Fechadeactualisacion='', Estado="", GPIO=list(), IMG=''):
+        super(Sensor, self).__init__('DB/sensores.json')
         #
-        self.idSensor = 0
-        self.idUsuario = 0
-        self.NombreSensor = ""
-        self.Descripcion = ""
-        self.Fechadecreacion = ""
-        self.Fechadeactualisacion = ""
-        self.Estados = ["indefinido", "habilitado", "deshabilitado"]
-        self.Estado = self.Estados[0]
-        self.GPIO = list()
-        self.IMG = ""
+        self.idSensor = idSensor
+        self.idUsuario = idUsuario
+        self.NombreSensor = NombreSensor
+        self.Descripcion = Descripcion
+        self.Fechadecreacion = Fechadecreacion
+        self.Fechadeactualisacion = Fechadeactualisacion
+        
+        self.Estado =""
+        self.GPIO = GPIO
+        self.IMG = IMG
         #
         self.lista = list()
         pass
@@ -54,3 +56,48 @@ class Sensor(JsonFile):
             lista.append(Sensor(idSensor=x['idSensor'], idUsuario=x['idUsuario'], NombreSensor=x["NombreSensor"],
                                 Descripcion=x["Descripcion"], Estado=x["Estado"], GPIO=x["GPIO"], IMG=x["IMG"]))
         self.lista = lista
+
+    def iterar(v):
+        for val in list(v):
+            if isinstance(val, list):
+                for subval in list(val):
+                    print(subval)
+            else:
+                print(val)
+
+    def __str__(self):
+        return str(self.idSensor)+str(self.idUsuario)+self.NombreSensor+self.Descripcion+self.Fechadecreacion+self.Fechadeactualisacion+self.Estado+str(self.GPIO)+self.IMG
+    def typo(self):
+        return type(self.GPIO)
+
+    def getDictory(self):
+        return {
+            'idSensor': self.idSensor,
+            'idUsuario': self.idUsuario,
+            'NombreSensor': self.NombreSensor,
+            'Descripcion': self.Descripcion,
+            'Fechadecreacion': self.Fechadecreacion,
+            'Fechadeactualisacion': self.Fechadeactualisacion,
+            'Estado': self.Estado,
+            'GPIO': self.GPIO,
+            'IMG': self.IMG
+        }
+
+    def listDict(self):
+        listDiccionario = list()
+        for x in self.lista:
+            listDiccionario.append(x.__dict__)
+            print(x.getDictory())
+        return listDiccionario
+
+    def __iter__(self):
+        self.__idx__ = 0
+        return self
+
+    def __next__(self):
+        if self.__idx__ < len(self.lista):
+            x = self.lista[self.__idx__]
+            self.__idx__ += 1
+            return x
+        else:
+            raise StopIteration
